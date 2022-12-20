e713cce8b4174b657c74205d7d8856c1ac59c5d9
Jefferson.art <canocs3@live.com>
2022-12-20 12:07:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+ ![Captura](https://user-images.githubusercontent.com/71351421/208697540-7750dbc8-72d9-4c04-90d4-e97ff439c59c.PNG)
+# Encriptador de texto
+
+_El reto consiste en crear una página web que cifre y descifre el texto que será ingresado por el usuario y mostrarlo en su forma cifrada o descifrada. El objetivo de este reto es evaluar los conocimientos adquiridos durante el curso "Principiante en programación"._
+
+
+
+## 🔧 Construido 
+
+* ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+* ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+* ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+## 🔑 Llaves de encriptación
+
+
+
+Las llaves de encriptación solicitadas son las siguientes:
+
+* La letra "a" es convertida a "ai".
+* La letra "e" es convertida a "enter".
+* La letra "i" es convertida a "imes".
+* La letra "o" es convertida a "ober"
+* La letra "u" es convertida a "ufat"
+
+
+## ✔️ Requisitos del desafío 
+
+
+* _Debe funcionar solo con letras minúsculas_
+* _No deben ser utilizados letras con acentos ni caracteres especiales_
+* _Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original._
+```
+Por ejemplo:
+
+"gato" => "gaitober"
+
+gaitober" => "gato"
+```
+
+* _La página debe tener campos para inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+* El resultado debe ser mostrado en la pantalla._
+
+### Extras:
+
+* _Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones._
+
+## Contact
+
+linkedin - [https://www.linkedin.com/in/jefferson-david-cano-hernandez/](https://www.linkedin.com/in/jefferson-david-cano-hernandez/) 
+
+Project Link: [https://github.com/JeffersonD-art/challenge-one-encriptador](https://github.com/JeffersonD-art/challenge-one-encriptador)
---
+++ main.js
@@ -0,0 +1,115 @@
+const botonE = document.getElementById("botonEnc");
+const botonD = document.getElementById("botonDec");
+const botonC = document.getElementById("botonCop");
+const botonP = document.getElementById("botonCop");
+const botonB = document.getElementById("botonborrar");
+var mayus = /[A-Z]/g;
+var caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
+
+function img() {
+    var textoVacio = "";
+    let textoT = document.getElementById("tArea").value;
+    textoT;
+    if (textoVacio !== textoT) {
+        document.getElementById("tapa").style.display = "none";
+    } else document.getElementById("tapa").style.display = "";
+}
+
+function limpiar() {
+    document.getElementById("texto").value = "";
+    document.getElementById("tArea").value = "";
+}
+
+function foco() {
+    document.getElementById("texto").focus();
+}
+
+function encriptar() {
+    let nuevoTexto = document.getElementById("texto").value;
+    nuevoTexto;
+    nuevoTexto = nuevoTexto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+
+    nuevoTexto;
+
+    document.getElementById("tArea").value = nuevoTexto;
+    document.getElementById("tArea").style.color = "#495057";
+    img();
+}
+
+function desencriptar() {
+    let nuevoTexto = document.getElementById("texto").value;
+    nuevoTexto;
+    nuevoTexto = nuevoTexto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+
+    nuevoTexto;
+
+    document.getElementById("tArea").value = nuevoTexto;
+    document.getElementById("tArea").style.color = "#495057";
+    img();
+}
+
+function copiar() {
+    document.getElementById("texto").placeholder = "";
+    let textCopi = document.getElementById("tArea");
+    textCopi.select();
+    document.execCommand("copy");
+    document.getElementById("texto").value = "";
+    document.getElementById("tArea");
+    document.getElementById("tArea").style.color = "limegreen";
+    document.getElementById("tArea").value = "Texto copiado";
+
+    foco();
+}
+
+function mayusculas() {
+    let nuevomensaje = document.getElementById("texto").value;
+    if (nuevomensaje.match(mayus) != nuevomensaje.match(mayus)) {
+        document.getElementById("texto").placeholder =
+            "no se permiten mayusculas";
+        limpiar();
+        foco();
+    }
+}
+
+function caracter() {
+    let nuevomensaje = document.getElementById("texto").value;
+    if (nuevomensaje.match(caracteres) != nuevomensaje.match(caracteres)) {
+        document.getElementById("texto").placeholder =
+            "No deben ser utilizados letras con acentos ni caracteres especiales";
+        limpiar();
+        foco();
+        limpiar();
+    }
+}
+
+function borrar() {
+    document.getElementById("texto").placeholder = "Ingrese el texto aqui";
+    document.getElementById("tArea").placeholder = "";
+    document.getElementById("tArea").style.color = "#495057";
+    limpiar();
+    foco();
+    img();
+}
+
+foco();
+botonE.addEventListener("click", mayusculas);
+botonE.addEventListener("click", caracter);
+botonE.addEventListener("click", encriptar);
+
+botonD.addEventListener("click", mayusculas);
+botonD.addEventListener("click", caracteres);
+botonD.addEventListener("click", desencriptar);
+
+botonC.addEventListener("click", copiar);
+botonB.addEventListener("click", borrar);
+
